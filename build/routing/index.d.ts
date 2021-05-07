debf428aca799ec7f6ddcc6c816b2090d13f298f
Joel Ek <contact@joelek.se>
2021-05-07 09:01:56 +0200

    Commit build artifacts.

--- build/routing/index.d.ts
+++ build/routing/index.d.ts
@@ -5,18 +5,25 @@ export declare type MessageObserver<A extends Message> = {
 export declare type MessageMap<A extends MessageMap<A>> = {
     [B in keyof A]: Message;
 };
+export declare type MessageObserverMap<A extends MessageMap<A>> = {
+    [B in keyof A]: Set<MessageObserver<A[B]>>;
+};
 export declare class MessageRouter<A extends MessageMap<A>> {
-    protected observers: Map<keyof A, Set<MessageObserver<A[keyof A]>>>;
+    protected observers: MessageObserverMap<A>;
     constructor();
     addObserver<B extends keyof A>(type: B, observer: MessageObserver<A[B]>): void;
     removeObserver<B extends keyof A>(type: B, observer: MessageObserver<A[B]>): void;
     route<B extends keyof A>(type: B, message: A[B]): void;
+    size(): number;
 }
 export declare type NamespacedMessageMap<A extends NamespacedMessageMap<A>> = {
     [B in keyof A]: MessageMap<A[B]>;
 };
+export declare type MessageRouterMap<A extends NamespacedMessageMap<A>> = {
+    [B in keyof A]: MessageRouter<A[B]>;
+};
 export declare class NamespacedMessageRouter<A extends NamespacedMessageMap<A>> {
-    protected routers: Map<keyof A, MessageRouter<A[keyof A]>>;
+    protected routers: MessageRouterMap<A>;
     constructor();
     addObserver<B extends keyof A, C extends keyof A[B]>(namespace: B, type: C, observer: MessageObserver<A[B][C]>): void;
     removeObserver<B extends keyof A, C extends keyof A[B]>(namespace: B, type: C, observer: MessageObserver<A[B][C]>): void;
--- build/routing/index.js
+++ build/routing/index.js
@@ -3,53 +3,62 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NamespacedMessageRouter = exports.MessageRouter = void 0;
 class MessageRouter {
     constructor() {
-        this.observers = new Map();
+        this.observers = Object.create(null);
     }
     addObserver(type, observer) {
-        let observers = this.observers.get(type);
+        let observers = this.observers[type];
         if (observers === undefined) {
             observers = new Set();
-            this.observers.set(type, observers);
+            this.observers[type] = observers;
         }
         observers.add(observer);
     }
     removeObserver(type, observer) {
-        let observers = this.observers.get(type);
+        let observers = this.observers[type];
         if (observers !== undefined) {
             observers.delete(observer);
+            if (observers.size === 0) {
+                delete this.observers[type];
+            }
         }
     }
     route(type, message) {
-        let observers = this.observers.get(type);
+        let observers = this.observers[type];
         if (observers !== undefined) {
             for (let observer of observers) {
                 observer(message);
             }
         }
     }
+    size() {
+        return Object.keys(this.observers).length;
+    }
 }
 exports.MessageRouter = MessageRouter;
 ;
 class NamespacedMessageRouter {
     constructor() {
-        this.routers = new Map();
+        this.routers = Object.create(null);
     }
     addObserver(namespace, type, observer) {
-        let router = this.routers.get(namespace);
+        let router = this.routers[namespace];
         if (router === undefined) {
             router = new MessageRouter();
-            this.routers.set(namespace, router);
+            this.routers[namespace] = router;
         }
         router.addObserver(type, observer);
     }
     removeObserver(namespace, type, observer) {
-        let router = this.routers.get(namespace);
+        let router = this.routers[namespace];
         if (router !== undefined) {
             router.removeObserver(type, observer);
+            if (router.size() === 0) {
+                delete this.routers[namespace];
+            }
         }
     }
     route(namespace, type, message) {
-        let router = this.routers.get(namespace);
+        let router = this.routers[namespace];
         if (router !== undefined) {
             router.route(type, message);
         }
