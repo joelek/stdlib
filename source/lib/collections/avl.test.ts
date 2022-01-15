a9afb57a84f7101605dd6e9087a4610da67bde52
Joel Ek <contact@joelek.se>
2022-01-15 17:53:22 +0100

    Add filterable AVL-tree.

---
+++ source/lib/collections/avl.test.ts
@@ -0,0 +1,547 @@
+import * as avl from "./avl";
+
+const Assert = {
+	true(condition: boolean, message: string = ""): void {
+		if (!condition) {
+			throw message;
+		}
+	},
+	array: {
+		equal<A>(one: Array<A>, two: Array<A>): void {
+			if (one.length !== two.length) {
+				throw `Expected arrays to be of equal length!`;
+			}
+			for (let i = 0; i < one.length; i++) {
+				if (one[i] !== two[i]) {
+					throw `Expected elements at index ${i} to be equal!`;
+				}
+			}
+		}
+	}
+};
+
+function log(string: string): void {
+	(globalThis as any).process.stdout.write(string);
+};
+
+function test(name: string, cb: (assert: typeof Assert) => Promise<any>): void {
+	cb(Assert).catch((error) => {
+		log(name);
+		log(error);
+	});
+};
+
+test(`It should compute tree balance.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 1);
+	let n3 = new avl.Node(3, null, 3);
+	let n4 = new avl.Node(4, null, 1);
+	let n5 = new avl.Node(5, null, 2);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n1.computeBalance() === 1);
+	assert.true(n2.computeBalance() === 0);
+	assert.true(n3.computeBalance() === 0);
+	assert.true(n4.computeBalance() === 0);
+	assert.true(n5.computeBalance() === -1);
+});
+
+test(`It should compute tree height.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 1);
+	let n3 = new avl.Node(3, null, 3);
+	let n4 = new avl.Node(4, null, 1);
+	let n5 = new avl.Node(5, null, 2);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n1.computeHeight() === 2);
+	assert.true(n2.computeHeight() === 1);
+	assert.true(n3.computeHeight() === 3);
+	assert.true(n4.computeHeight() === 1);
+	assert.true(n5.computeHeight() === 2);
+});
+
+test(`It should support filtering nodes without using a filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter()).map((entry) => entry.key);
+	let expected = [1, 2, 3, 4, 5] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should support filtering nodes using a ">" filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter({ operator: ">", key: 1 }))
+		.map((entry) => entry.key);
+	let expected = [2, 3, 4, 5] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should support filtering nodes using a ">=" filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter({ operator: ">=", key: 1 }))
+		.map((entry) => entry.key);
+	let expected = [1, 2, 3, 4, 5] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should support filtering nodes using a "=" filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter({ operator: "=", key: 1 }))
+		.map((entry) => entry.key);
+	let expected = [1] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should support filtering nodes using a "<=" filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter({ operator: "<=", key: 5 }))
+		.map((entry) => entry.key);
+	let expected = [1, 2, 3, 4, 5] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should support filtering nodes using a "<" filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter({ operator: "<", key: 5 }))
+		.map((entry) => entry.key);
+	let expected = [1, 2, 3, 4] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should support filtering nodes using a ">" filter and a "<" filter.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	let observed = Array.from(n3.filter({ operator: ">", key: 1 }, { operator: "<", key: 5 }))
+		.map((entry) => entry.key);
+	let expected = [2, 3, 4] as Array<number>;
+	assert.array.equal(observed, expected);
+});
+
+test(`It should locate maximum nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n1.getMaximum() === n2);
+	assert.true(n2.getMaximum() === n2);
+	assert.true(n3.getMaximum() === n5);
+	assert.true(n4.getMaximum() === n4);
+	assert.true(n5.getMaximum() === n5);
+});
+
+test(`It should locate minimum nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n1.getMinimum() === n1);
+	assert.true(n2.getMinimum() === n2);
+	assert.true(n3.getMinimum() === n1);
+	assert.true(n4.getMinimum() === n4);
+	assert.true(n5.getMinimum() === n4);
+});
+
+test(`It should locate lower parent nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n2.getLowerParent() === n1);
+	assert.true(n4.getLowerParent() === n3);
+});
+
+test(`It should locate upper parent nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n2.getUpperParent() === n3);
+	assert.true(n4.getUpperParent() === n5);
+});
+
+test(`It should locate predecessor nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n1.getPredecessor() == null);
+	assert.true(n2.getPredecessor() === n1);
+	assert.true(n3.getPredecessor() === n2);
+	assert.true(n4.getPredecessor() === n3);
+	assert.true(n5.getPredecessor() === n4);
+});
+
+test(`It should locate successor nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	n1.setUpper(n2);
+	assert.true(n1.getSuccessor() === n2);
+	assert.true(n2.getSuccessor() === n3);
+	assert.true(n3.getSuccessor() === n4);
+	assert.true(n4.getSuccessor() === n5);
+	assert.true(n5.getSuccessor() == null);
+});
+
+test(`It should support inserting 1,2,3 in 1,2,3 order.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let r = n1;
+	r = r.insert(n2) ?? r;
+	r = r.insert(n3) ?? r;
+	assert.true(r === n2);
+	assert.true(r.getLower() === n1);
+	assert.true(r.getUpper() === n3);
+});
+
+test(`It should support inserting 1,2,3 in 1,3,2 order.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let r = n1;
+	r = r.insert(n3) ?? r;
+	r = r.insert(n2) ?? r;
+	assert.true(r === n2);
+	assert.true(r.getLower() === n1);
+	assert.true(r.getUpper() === n3);
+});
+
+test(`It should support inserting 1,2,3 in 2,1,3 order.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let r = n2;
+	r = r.insert(n1) ?? r;
+	r = r.insert(n3) ?? r;
+	assert.true(r === n2);
+	assert.true(r.getLower() === n1);
+	assert.true(r.getUpper() === n3);
+});
+
+test(`It should support inserting 1,2,3 in 2,3,1 order.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let r = n2;
+	r = r.insert(n3) ?? r;
+	r = r.insert(n1) ?? r;
+	assert.true(r === n2);
+	assert.true(r.getLower() === n1);
+	assert.true(r.getUpper() === n3);
+});
+
+test(`It should support inserting 1,2,3 in 3,1,2 order.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let r = n3;
+	r = r.insert(n1) ?? r;
+	r = r.insert(n2) ?? r;
+	assert.true(r === n2);
+	assert.true(r.getLower() === n1);
+	assert.true(r.getUpper() === n3);
+});
+
+test(`It should support inserting 1,2,3 in 3,2,1 order.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let r = n3;
+	r = r.insert(n2) ?? r;
+	r = r.insert(n1) ?? r;
+	assert.true(r === n2);
+	assert.true(r.getLower() === n1);
+	assert.true(r.getUpper() === n3);
+});
+
+test(`It should support removing nodes with no children.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 1);
+	let n3 = new avl.Node(3, null, 3);
+	let n4 = new avl.Node(4, null, 2);
+	let n5 = new avl.Node(5, null, 1);
+	n1.setUpper(n2);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	let result = n3.remove(2);
+	assert.true(result === n3);
+	assert.true(n1.getUpper() == null);
+});
+
+test(`It should support removing nodes with one lower child.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 1);
+	let n3 = new avl.Node(3, null, 3);
+	let n4 = new avl.Node(4, null, 2);
+	let n5 = new avl.Node(5, null, 1);
+	n1.setUpper(n2);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	let result = n3.remove(5);
+	assert.true(result === n3);
+	assert.true(n3.getUpper() === n4);
+});
+
+test(`It should support removing nodes with one upper child.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 1);
+	let n3 = new avl.Node(3, null, 3);
+	let n4 = new avl.Node(4, null, 2);
+	let n5 = new avl.Node(5, null, 1);
+	n1.setUpper(n2);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	let result = n3.remove(1);
+	assert.true(result === n3);
+	assert.true(n3.getLower() === n2);
+});
+
+test(`It should support removing nodes with two children through substitution.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 1);
+	let n3 = new avl.Node(3, null, 3);
+	let n4 = new avl.Node(4, null, 2);
+	let n5 = new avl.Node(5, null, 1);
+	n1.setUpper(n2);
+	n3.setLower(n1);
+	n3.setUpper(n5);
+	n5.setLower(n4);
+	let result = n3.remove(3);
+	assert.true(result === n3);
+	assert.true(result?.entry().key === 4);
+	assert.true(n3.getLower() === n1);
+	assert.true(n3.getUpper() === n5);
+});
+
+test(`It should rebalance left left heavy nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 2);
+	let n2 = new avl.Node(2, null, 3);
+	let n3 = new avl.Node(3, null, 1);
+	let n4 = new avl.Node(4, null, 4);
+	let n5 = new avl.Node(5, null, 1);
+	n4.setLower(n2);
+	n4.setUpper(n5);
+	n2.setLower(n1);
+	n2.setUpper(n3);
+	let result = n4.rebalance();
+	assert.true(result === n2);
+	assert.true(n2.getLower() === n1);
+	assert.true(n2.getUpper() === n4);
+	assert.true(n4.getLower() === n3);
+	assert.true(n4.getUpper() === n5);
+});
+
+test(`It should rebalance left right heavy nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 1);
+	let n2 = new avl.Node(2, null, 3);
+	let n3 = new avl.Node(3, null, 2);
+	let n4 = new avl.Node(4, null, 4);
+	let n5 = new avl.Node(5, null, 1);
+	n4.setLower(n2);
+	n4.setUpper(n5);
+	n2.setLower(n1);
+	n2.setUpper(n3);
+	let result = n4.rebalance();
+	assert.true(result === n3);
+	assert.true(n3.getLower() === n2);
+	assert.true(n3.getUpper() === n4);
+	assert.true(n2.getLower() === n1);
+	assert.true(n4.getUpper() === n5);
+});
+
+test(`It should rebalance right left heavy nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 1);
+	let n2 = new avl.Node(2, null, 4);
+	let n3 = new avl.Node(3, null, 2);
+	let n4 = new avl.Node(4, null, 3);
+	let n5 = new avl.Node(5, null, 1);
+	n2.setLower(n1);
+	n2.setUpper(n4);
+	n4.setLower(n3);
+	n4.setUpper(n5);
+	let result = n2.rebalance();
+	assert.true(result === n3);
+	assert.true(n3.getLower() === n2);
+	assert.true(n3.getUpper() === n4);
+	assert.true(n2.getLower() === n1);
+	assert.true(n4.getUpper() === n5);
+});
+
+test(`It should rebalance right right heavy nodes.`, async (assert) => {
+	let n1 = new avl.Node(1, null, 1);
+	let n2 = new avl.Node(2, null, 4);
+	let n3 = new avl.Node(3, null, 1);
+	let n4 = new avl.Node(4, null, 3);
+	let n5 = new avl.Node(5, null, 2);
+	n2.setLower(n1);
+	n2.setUpper(n4);
+	n4.setLower(n3);
+	n4.setUpper(n5);
+	let result = n2.rebalance();
+	assert.true(result === n4);
+	assert.true(n4.getLower() === n2);
+	assert.true(n4.getUpper() === n5);
+	assert.true(n2.getLower() === n1);
+	assert.true(n2.getUpper() === n3);
+});
+
+test(`It should perform left rotations.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n2.setLower(n1);
+	n2.setUpper(n4);
+	n4.setLower(n3);
+	n4.setUpper(n5);
+	let result = n2.rotateLeft();
+	assert.true(result === n4);
+	assert.true(n4.getLower() === n2);
+	assert.true(n4.getUpper() === n5);
+	assert.true(n2.getLower() === n1);
+	assert.true(n2.getUpper() === n3);
+	assert.true(n2.getHeight() === 2);
+	assert.true(n4.getHeight() === 3);
+});
+
+test(`It should perform right rotations.`, async (assert) => {
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	let n3 = new avl.Node(3, null);
+	let n4 = new avl.Node(4, null);
+	let n5 = new avl.Node(5, null);
+	n4.setLower(n2);
+	n4.setUpper(n5);
+	n2.setLower(n1);
+	n2.setUpper(n3);
+	let result = n4.rotateRight();
+	assert.true(result === n2);
+	assert.true(n2.getLower() === n1);
+	assert.true(n2.getUpper() === n4);
+	assert.true(n4.getLower() === n3);
+	assert.true(n4.getUpper() === n5);
+	assert.true(n4.getHeight() === 2);
+	assert.true(n2.getHeight() === 3);
+});
+
+test(`It should update all pointers when setting the lower child.`, async (assert) => {
+	let n0 = new avl.Node(0, null);
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	n1.setLower(n0);
+	assert.true(n0.getParent() === n1);
+	assert.true(n1.getLower() === n0);
+	assert.true(n2.getLower() === undefined);
+	n2.setLower(n0);
+	assert.true(n0.getParent() === n2);
+	assert.true(n1.getLower() === undefined);
+	assert.true(n2.getLower() === n0);
+});
+
+test(`It should update all pointers when setting the upper child.`, async (assert) => {
+	let n0 = new avl.Node(0, null);
+	let n1 = new avl.Node(1, null);
+	let n2 = new avl.Node(2, null);
+	n1.setUpper(n0);
+	assert.true(n0.getParent() === n1);
+	assert.true(n1.getUpper() === n0);
+	assert.true(n2.getUpper() === undefined);
+	n2.setUpper(n0);
+	assert.true(n0.getParent() === n2);
+	assert.true(n1.getUpper() === undefined);
+	assert.true(n2.getUpper() === n0);
+});
---
+++ source/lib/collections/avl.ts
@@ -0,0 +1,385 @@
+export type Entry<A> = {
+	key: number;
+	value: A;
+};
+
+export type Operator = "<" | "<=" | "=" | ">=" | ">";
+
+export type Filter = {
+	operator: Operator;
+	key: number;
+};
+
+export class Node<A> {
+	private key: number;
+	private value: A;
+	private height: number;
+	private parent: Node<A> | undefined;
+	private lower: Node<A> | undefined;
+	private upper: Node<A> | undefined;
+
+	constructor(key: number, value: A, height?: number) {
+		this.key = key;
+		this.value = value;
+		this.height = height ?? 1;
+		this.parent = undefined;
+		this.lower = undefined;
+		this.upper = undefined;
+	}
+
+	compare(filter: Filter): number | undefined {
+		let operator = filter.operator;
+		let key = filter.key;
+		if (operator === "<") {
+			if (key < this.key) {
+				return;
+			} else if (key > this.key) {
+				return 0;
+			} else {
+				return -1;
+			}
+		} else if (operator === "<=") {
+			if (key < this.key) {
+				return;
+			} else if (key > this.key) {
+				return 0;
+			} else {
+				return 0;
+			}
+		} else if (operator === "=") {
+			if (key < this.key) {
+				return -1;
+			} else if (key > this.key) {
+				return 1;
+			} else {
+				return 0;
+			}
+		} else if (operator === ">=") {
+			if (key < this.key) {
+				return 0;
+			} else if (key > this.key) {
+				return;
+			} else {
+				return 0;
+			}
+		} else if (operator === ">") {
+			if (key < this.key) {
+				return 0;
+			} else if (key > this.key) {
+				return;
+			} else {
+				return 1;
+			}
+		}
+	}
+
+	computeBalance(): number {
+		return (this.upper?.height ?? 0) - (this.lower?.height ?? 0);
+	}
+
+	computeHeight(): number {
+		return Math.max((this.lower?.height ?? 0), (this.upper?.height ?? 0)) + 1;
+	}
+
+	entry(): Entry<A> {
+		return {
+			key: this.key,
+			value: this.value
+		};
+	}
+
+	* filter(...filters: Array<Filter>): Iterable<Entry<A>> {
+		let lower = true;
+		let current = true;
+		let upper = true;
+		for (let filter of filters) {
+			let comparison = this.compare(filter);
+			if (comparison == null) {
+				return;
+			}
+			lower = lower && comparison <= 0;
+			current = current && comparison === 0;
+			upper = upper && comparison >= 0;
+		}
+		if (lower && this.lower != null) {
+			yield * this.lower.filter(...filters);
+		}
+		if (current) {
+			yield this.entry();
+		}
+		if (upper && this.upper != null) {
+			yield * this.upper.filter(...filters);
+		}
+	}
+
+	getMaximum(): Node<A> {
+		let node = this as Node<A>;
+		while (node.upper != null) {
+			node = node.upper;
+		}
+		return node;
+	}
+
+	getMinimum(): Node<A> {
+		let node = this as Node<A>;
+		while (node.lower != null) {
+			node = node.lower;
+		}
+		return node;
+	}
+
+	getLowerParent(): Node<A> | undefined {
+		let parent = this.parent;
+		let child = this as Node<A>;
+		while (parent != null && child === parent.lower) {
+			child = parent;
+			parent = parent.parent;
+		}
+		return parent;
+	}
+
+	getUpperParent(): Node<A> | undefined {
+		let parent = this.parent;
+		let child = this as Node<A>;
+		while (parent != null && child === parent.upper) {
+			child = parent;
+			parent = parent.parent;
+		}
+		return parent;
+	}
+
+	getPredecessor(): Node<A> | undefined {
+		if (this.lower != null) {
+			return this.lower.getMaximum();
+		}
+		return this.getLowerParent();
+	}
+
+	getSuccessor(): Node<A> | undefined {
+		if (this.upper != null) {
+			return this.upper.getMinimum();
+		}
+		return this.getUpperParent();
+	}
+
+	insert(node: Node<A>): Node<A> {
+		if (node.key === this.key) {
+			this.value = node.value;
+			return this;
+		}
+		if (node.key < this.key) {
+			if (this.lower == null) {
+				this.setLower(node);
+			} else {
+				this.setLower(this.lower.insert(node));
+			}
+		} else {
+			if (this.upper == null) {
+				this.setUpper(node);
+			} else {
+				this.setUpper(this.upper.insert(node));
+			}
+		}
+		this.setHeight(this.computeHeight());
+		return this.rebalance();
+	}
+
+	rebalance(): Node<A> {
+		let balance = this.computeBalance();
+		if (balance < -1) {
+			if (this.lower == null) {
+				throw `Expected lower child to be non-null!`;
+			}
+			let balance = this.lower.computeBalance();
+			if (balance <= 0) {
+				return this.rotateRight();
+			} else {
+				this.setLower(this.lower.rotateLeft());
+				return this.rotateRight();
+			}
+		}
+		if (balance > 1) {
+			if (this.upper == null) {
+				throw `Expected upper child to be non-null!`;
+			}
+			let balance = this.upper.computeBalance();
+			if (balance >= 0) {
+				return this.rotateLeft();
+			} else {
+				this.setUpper(this.upper.rotateRight());
+				return this.rotateLeft();
+			}
+		}
+		return this;
+	}
+
+	remove(key: number): Node<A> | undefined {
+		if (key === this.key) {
+			if (this.lower != null) {
+				if (this.upper != null) {
+					let minimum = this.upper.getMinimum();
+					let key = minimum.key;
+					this.setUpper(this.upper.remove(key));
+					this.key = key;
+					this.setHeight(this.computeHeight());
+					return this.rebalance();
+				} else {
+					return this.lower;
+				}
+			} else {
+				if (this.upper != null) {
+					return this.upper;
+				} else {
+					return;
+				}
+			}
+		}
+		if (key < this.key) {
+			if (this.lower != null) {
+				this.setLower(this.lower.remove(key));
+			} else {
+				return this;
+			}
+		} else {
+			if (this.upper != null) {
+				this.setUpper(this.upper.remove(key));
+			} else {
+				return this;
+			}
+		}
+		this.setHeight(this.computeHeight());
+		return this.rebalance();
+	}
+
+	rotateLeft(): Node<A> {
+		let upper = this.upper;
+		if (upper == null) {
+			throw `Expected upper child to be non-null!`;
+		}
+		this.setUpper(upper.lower);
+		upper.setLower(this);
+		this.setHeight(this.computeHeight());
+		upper.setHeight(upper.computeHeight());
+		return upper;
+	}
+
+	rotateRight(): Node<A> {
+		let lower = this.lower;
+		if (lower == null) {
+			throw `Expected lower child to be non-null!`;
+		}
+		this.setLower(lower.upper);
+		lower.setUpper(this);
+		this.setHeight(this.computeHeight());
+		lower.setHeight(lower.computeHeight());
+		return lower;
+	}
+
+	getHeight(): number {
+		return this.height;
+	}
+
+	setHeight(height: number): number {
+		return this.height = height;
+	}
+
+	getParent(): Node<A> | undefined {
+		return this.parent;
+	}
+
+	setParent(parent: Node<A> | undefined): void {
+		if (this.parent != null) {
+			if (this.parent.lower === this) {
+				this.parent.lower = undefined;
+			} else if (this.parent.upper === this) {
+				this.parent.upper = undefined;
+			}
+		}
+		this.parent = parent;
+	}
+
+	getLower(): Node<A> | undefined {
+		return this.lower;
+	}
+
+	setLower(lower: Node<A> | undefined): void {
+		if (lower != null) {
+			lower.setParent(this);
+		}
+		if (this.lower != null) {
+			this.lower.setParent(undefined);
+		}
+		this.lower = lower;
+	}
+
+	getUpper(): Node<A> | undefined {
+		return this.upper;
+	}
+
+	setUpper(upper: Node<A> | undefined): void {
+		if (upper != null) {
+			upper.setParent(this);
+		}
+		if (this.upper != null) {
+			this.upper.setParent(undefined);
+		}
+		this.upper = upper;
+	}
+};
+
+export class Tree<A> {
+	private root: Node<A> | undefined;
+
+	constructor() {
+		this.root = undefined;
+	}
+
+	* [Symbol.iterator](): Iterable<Entry<A>> {
+		yield * this.filter();
+	}
+
+	clear(): void {
+		this.root = undefined;
+	}
+
+	* filter(...filters: Filter[]): Iterable<Entry<A>> {
+		if (this.root == null) {
+			return;
+		}
+		for (let node of this.root.filter(...filters)) {
+			yield {
+				key: node.key,
+				value: node.value
+			};
+		}
+	}
+
+	insert(key: number, value: A): void {
+		let node = new Node<A>(key, value);
+		if (this.root != null) {
+			this.root = this.root.insert(node);
+		} else {
+			this.root = node;
+		}
+	}
+
+	length(): number {
+		let length = 0;
+		for (let entry in this) {
+			length += 1;
+		}
+		return length;
+	}
+
+	lookup(filter: Filter): Entry<A> | undefined {
+		for (let entry of this.filter(filter)) {
+			return entry;
+		}
+	}
+
+	remove(key: number): void {
+		if (this.root != null) {
+			this.root = this.root.remove(key);
+		}
+	}
+};
